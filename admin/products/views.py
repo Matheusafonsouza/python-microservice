9a481877066505bd736918798c3189ddaca3bdcf
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-16 00:31:09 -0300

    feat: product view with list method

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request):
+        pass
+
+    def update(self, request):
+        pass
+
+    def destroy(self, request):
+        pass
