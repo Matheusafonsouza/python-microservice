a066c61071fe3be3a101dcc729de5685f94577a5
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-16 00:39:57 -0300

    feat: product update and destroy methods

--- admin/products/views.py
+++ admin/products/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
 
 
 class ProductViewSet(viewsets.ViewSet):
@@ -17,12 +18,18 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
-        product = Product.objects.get(pk=pk)
+        product = get_object_or_404(Product, pk=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
     def update(self, request, pk=None):
-        pass
+        product = get_object_or_404(Product, pk=pk)
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
-        pass
+        product = get_object_or_404(Product, pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
