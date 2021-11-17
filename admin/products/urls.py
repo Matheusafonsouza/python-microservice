0d3a851c4ea58439dceff0f0abec1b7bb207d96d
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-16 23:30:27 -0300

    feat: using model view set

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path
 from products.views import ProductViewSet
+from rest_framework import routers
 
-urlpatterns = [
-    path('products', ProductViewSet.as_view({
-        'get': 'list',
-        'post': 'create'
-    })),
-    path('products/<str:pk>', ProductViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update',
-        'delete': 'destroy',
-    }))
-]
+
+router = routers.SimpleRouter()
+router.register('products', ProductViewSet)
+
+urlpatterns = [] + router.urls
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,35 +1,11 @@
-from rest_framework import viewsets, status
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
 from django.shortcuts import get_object_or_404
 
 
-class ProductViewSet(viewsets.ViewSet):
-    def list(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def retrieve(self, request, pk=None):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-
-    def destroy(self, request, pk=None):
-        product = get_object_or_404(Product, pk=pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
