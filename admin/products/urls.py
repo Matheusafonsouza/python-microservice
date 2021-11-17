81c1da0afbb5350fea2808bff7757897cbba8b56
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-16 23:32:36 -0300

    feat: users serializer and views

--- admin/products/urls.py
+++ admin/products/urls.py
@@ -1,8 +1,10 @@
-from products.views import ProductViewSet
+from products.views import ProductViewSet, UserViewSet
 from rest_framework import routers
 
 
 router = routers.SimpleRouter()
+
 router.register('products', ProductViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [] + router.urls
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,11 +1,13 @@
-from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
-from products.models import Product
-from products.serializers import ProductSerializer
-from django.shortcuts import get_object_or_404
+from products.models import Product, User
+from products.serializers import ProductSerializer, UserSerializer
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
