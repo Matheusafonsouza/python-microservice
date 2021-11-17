126835174ae994257fda9eff13a57b594d34eef1
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-16 23:54:21 -0300

    feat: migrate and manager

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
