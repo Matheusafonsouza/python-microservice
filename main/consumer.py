1c1c8c40c664a6ff99b32ce871705936b09f5b8a
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-20 01:49:21 -0300

    chore: flask callback methods

--- main/consumer.py
+++ main/consumer.py
@@ -1,4 +1,6 @@
 import pika
+import json
+from main import Product, db
 
 params = pika.URLParameters(
     'amqps://uswndzhq:DT0zKsVmkDKO1GlhfW4Y5NLI-JqV5o_Z@woodpecker.rmq.cloudamqp.com/uswndzhq')
@@ -10,7 +12,25 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(
+            id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data.get['title']
+        product.image = data.get['image']
+        db.session.commit()
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
 
 
 channel.basic_consume(queue='main', on_message_callback=callback)
